44ea14983a1485741f788ca8e83a9598ebddddd5
Gerald Watts <geraldwattsthethird@gmail.com>
2021-01-19 22:26:22 -0600

    CSV updates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,41 @@
+# Import os and csv
+import os
+import csv
 
+# Set CSV file
+
+PyPoll_csv = os.path.join("Resources", "election_data.csv")
+
+# Create variables to store lists
+
+candidate = []
+votes = []
+voters = []
+votepercentage = []
+
+# Count the votes
+
+count = 0
+
+# Open CSV for reading
+
+with open(PyPoll_csv, "r") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+
+    # Set the loops
+
+    for row in csv_reader:
+
+        count = count + 1
+        candidate.append(row[2])
+
+    for x in set(candidate):
+            
+        unique_candidate.append(x)
+        y = candidate.count(x)
+        votes.append(y)
+        z = (y/count)*100
+        votepercentage.append(z)
+
+    
